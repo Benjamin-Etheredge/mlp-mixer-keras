3b5a0d3d38bd85810c55a4a5129a57828137ba6c
ben <benjamin.etheredge@gmail.com>
2021-05-10 01:59:05 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mlp-mixer-keras",
-    version="0.0.2",
+    version="0.0.3",
     license='MIT',
     author="Benjamin Etheredge",
     author_email="",
